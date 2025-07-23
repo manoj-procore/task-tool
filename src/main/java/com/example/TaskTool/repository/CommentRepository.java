a2759384e50eb05e3521861a9a9f9341338924bb
manoj-procore <manoj.n@procore.com>
2025-07-23 09:36:17 +0530

    Controller and Repository

--- src/main/java/com/example/TaskTool/repository/CommentRepository.java
+++ src/main/java/com/example/TaskTool/repository/CommentRepository.java
@@ -1,9 +1,8 @@
 package com.example.TaskTool.repository;
-
-import com.example.TaskTool.model.Task;
+import com.example.TaskTool.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class CommentRepository extends JpaRepository<Task,Long> {
+public interface CommentRepository extends JpaRepository<Comment,Long> {
 }
